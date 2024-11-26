97fd1b44b01c8f2ac8a2533b02c1272a84c1128b
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2024-11-26 11:27:51 +0000

    refactor(Person): change 'Person' class to record.

--- src/main/java/com/northcoders/demospringbootapp/model/Person.java
+++ src/main/java/com/northcoders/demospringbootapp/model/Person.java
@@ -1,4 +1,4 @@
 package com.northcoders.demospringbootapp.model;
 
-public record Person() {
+public record Person(String name, int age, String email, String place_of_birth, String favourite_food) {
 }
