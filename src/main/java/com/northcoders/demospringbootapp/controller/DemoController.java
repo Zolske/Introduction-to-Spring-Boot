f0340dd8490dc87b39ac851c647c97aba8795715
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2024-11-26 15:07:01 +0000

    feat(city): returns jason.

--- src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
+++ src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
@@ -1,8 +1,12 @@
 package com.northcoders.demospringbootapp.controller;
 import com.northcoders.demospringbootapp.model.Person;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("api/v1")
@@ -17,5 +21,20 @@ public class DemoController {
     @GetMapping("/person")
     public Person getPerson() {
         return person;
+    }
+
+//    public  String getCity(){
+//        WebClient client = WebClient.create("https://geocoding-api.open-meteo.com/v1/search");
+//
+//    }
+
+    @GetMapping("/city_{city_name}")
+    public String city(@PathVariable String city_name){
+        String uri = "https://geocoding-api.open-meteo.com/v1/search?name=" + city_name.toString();
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
+//        return "yes";
     }
 }
